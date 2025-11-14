bc7b5e5efb415ae8232a3559b1da72b92d1ffeb7
Zeba <saidzodazebida@gmail.com>
2025-11-14 14:59:10 +0300

    third(enum)

--- app/src/main/java/com/example/genericsobjectsextensions/MainActivity.kt
+++ app/src/main/java/com/example/genericsobjectsextensions/MainActivity.kt
@@ -1,24 +1,20 @@
 package com.example.genericsobjectsextensions
 
 
-class FillInTheBlankQuestion(
+class Question<T>(
     val questionText: String,
-    val answer: String,
-    val difficulty: String
+    val answer: T,
+    val difficulty: Difficulty
 )
 
-class TrueOrFalseQuestion(
-    val questionText: String,
-    val answer: Boolean,
-    val difficulty: String
-)
-class NumericQuestion(
-    val questionText: String,
-    val answer: Int,
-    val difficulty: String
-)
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
 
 
 fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 
 }
