c57b898293c254ea0f651fbe23babed365b20e8b
Zeba <saidzodazebida@gmail.com>
2025-11-14 15:12:16 +0300

    fourth(singleton object)

--- app/src/main/java/com/example/genericsobjectsextensions/MainActivity.kt
+++ app/src/main/java/com/example/genericsobjectsextensions/MainActivity.kt
@@ -11,10 +11,26 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+
+
+
+
 
 fun main() {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-
+    println(question1.toString())
+    println("${Quiz.answered} of ${Quiz.total} answered.")
 }
