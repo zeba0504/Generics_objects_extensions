a9e7ba57c321bad6c0f418bd1ce797880503a628
Zeba <saidzodazebida@gmail.com>
2025-11-14 16:02:59 +0300

    fifth

--- app/src/main/java/com/example/genericsobjectsextensions/MainActivity.kt
+++ app/src/main/java/com/example/genericsobjectsextensions/MainActivity.kt
@@ -11,7 +11,13 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz : ProgressPrintable {
+
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
@@ -20,17 +26,42 @@ class Quiz {
         var total: Int = 10
         var answered: Int = 3
     }
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+    }
 }
 
 
 
 
-
-
 fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
-    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-    println(question1.toString())
-    println("${Quiz.answered} of ${Quiz.total} answered.")
+    val quiz = Quiz()
+    quiz.printQuiz()
+    Quiz().printProgressBar()
 }
